26a7914783b3cc0ef4e570413ae6cccb3c0b3097
mollylin5114 <mollylin5114@gmail.com>
2026-06-12 16:24:54 +0900

    Integrate PostHog for analytics tracking
    Initialize PostHog analytics and wrap the app in PostHogProvider.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
 import '../styles/globals.css'
 import { Analytics } from '@vercel/analytics/react'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+import { useEffect } from 'react'
+
+if (typeof window !== 'undefined') {
+  posthog.init('phc_xx9WQoWNfH4SX28J2LNpg9R2NxUf4VnJHxzTmEg6ZRj4', {
+    api_host: 'https://app.posthog.com',
+    capture_pageview: true,
+    capture_pageleave: true,
+    autocapture: false,
+  })
+}
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <PostHogProvider client={posthog}>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </PostHogProvider>
   )
 }
